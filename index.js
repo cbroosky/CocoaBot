d4599d74d9c803fc7e17931612d0bfbe33aa3cac
Carsten Brooks <cbroosk56@gmail.copm>
2021-11-08 22:39:05 -0700

    added basic testing calls

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const app = require("express")();
-const PORT = 8080;
+const express = require("express");
+const app = express();
+const { spawn } = require('child_process');
+const PORT = 3000;
+app.use(express.static(__dirname + "/public"));
 
-app.listen(
-	PORT,
-	() => console.log(`it's alive on https://localhost:${PORT}`)
-);
+app.get("/hello", function (req, res) {
+    // res.writeHead(200);
+    var dataToSend = "";
+    const hellocode = spawn("python", ['scripts/helloworld.py']);
+    hellocode.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+       });
+    hellocode.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        console.log(dataToSend);
+        // send data to browser
+        res.send(dataToSend);
+    });
+});
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
